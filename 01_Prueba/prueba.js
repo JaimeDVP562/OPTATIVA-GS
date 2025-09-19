0efc9392eeddd968836ff6adaae37f2e3bed4cf5
Jaime Gavilan Torrero <jgavtor562@g.educaand.es>
2025-09-19 20:09:24 +0200

    " "

---
+++ 01_Prueba/prueba.js
@@ -0,0 +1,11 @@
+
+// Declaramos las variables
+
+  nombre1 = prompt("Introduce tu nombre");
+  nombre2 = prompt("Introduce de quien te guste");
+  
+// Mostramos por pantalla el mensaje de alerta para que el ususario introduzca los datos 
+ alert(nombre1 + " X " + nombre2 + "<3");
+
+// Imprimimos por pantalla el resultado
+ document.writeln(nombre1 + " y " + nombre2 + " son novios");
