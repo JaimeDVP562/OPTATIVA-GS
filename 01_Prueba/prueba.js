77ea2798e1cd3699eac45b485095ab6d26b2a93d
Jaime Gavilan Torrero <jgavtor562@g.educaand.es>
2025-09-19 20:39:39 +0200

    " "

--- 01_Prueba/prueba.js
+++ 01_Prueba/prueba.js
@@ -1,11 +1,35 @@
+/**
+ * 
+ * 
+ * 
+ * // Ejercicio 01
 
+// Declaramos las variables
+
+  nombre1 = prompt("Introduce tu nombre");
+  nombre2 = prompt("Introduce de quien te guste");
+  
+// Mostramos por pantalla el mensaje de alerta para que el ususario introduzca los datos 
+ alert(nombre1 + " X " + nombre2 + "<3");
+
+// Imprimimos por pantalla el resultado
+ document.writeln(nombre1 + " y " + nombre2 + " son novios");
+ */
 
 // Ejercicio 02
  
   
-  A = prompt("Introduce numero 1");
-  B = prompt("Introduce numero 2");
+  numero1 = prompt("Introduce numero 1");
+  numero2 = prompt("Introduce numero 2");
 
-  document.writeln ( A + B)
-  
-  
+  resultado = (parseInt(numero1) + parseInt(numero2));
+
+  if (numero1 == numero) {
+    
+  } else {
+    
+  }
+  numero1 == numero2 == ("false");
+
+  document.writeln(numero1  + " y " + numero2 + " es " + resultado); 
+
