8a3ab0230fae81ec348959c4911c04a28b100422
JaimeDVP562 <jgavtor562@g.educaand.es>
2025-09-19 20:27:48 +0200

    asdasd

--- 01_Prueba/prueba.js
+++ 01_Prueba/prueba.js
@@ -1,27 +1,11 @@
 
 
-
-// Ejercicio 01
-/*
-// Declaramos las variables
-
-  nombre1 = prompt("Introduce tu nombre");
-  nombre2 = prompt("Introduce de quien te guste");
-  
-// Mostramos por pantalla el mensaje de alerta para que el ususario introduzca los datos 
- alert(nombre1 + " X " + nombre2 + "<3");
-
-// Imprimimos por pantalla el resultado
- document.writeln(nombre1 + " y " + nombre2 + " son novios");
-
-
-*/
-
-
 // Ejercicio 02
  
-
+  
   A = prompt("Introduce numero 1");
   B = prompt("Introduce numero 2");
 
   document.writeln ( A + B)
+  
+  
